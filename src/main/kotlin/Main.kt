cc64346479f02c9e7e527aae909c033e60155932
adria.silgado.7e7 <adria.silgado.7e7@itb.cat>
2024-04-24 12:12:09 +0200

    acabé.

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,3 +1,4 @@
+import com.mongodb.MongoException
 import com.mongodb.reactivestreams.client.MongoClients
 import com.mongodb.reactivestreams.client.MongoCollection
 import org.bson.Document
@@ -6,32 +7,31 @@ import java.io.File
 
 
 fun main() {
-    // Connection to the MongoDB
-    val uri = "mongodb+srv://adriasilgado:123456@clusterpractica.muro8id.mongodb.net/?retryWrites=true&w=majority&appName=ClusterPractica"
-    val mongoClient = MongoClients.create(uri)
+    try {
+        // Connection to the MongoDB
+        val uri = "mongodb+srv://adriasilgado:adriasilgado@clusterpractica.muro8id.mongodb.net/?retryWrites=true&w=majority&appName=ClusterPractica"
+        val mongoClient = MongoClients.create(uri)
 
-    //Connection to the database
-    val dbItb = mongoClient.getDatabase("itb")
+        //Connection to the database
+        val dbItb = mongoClient.getDatabase("itb")
 
-    //Connection to the collections
-    val collectionRestaurants: MongoCollection<Document> = dbItb.getCollection("restaurants")
-    val collectionProducts : MongoCollection<Document> = dbItb.getCollection("products")
+        //Connection to the collections
+        val collectionRestaurants: MongoCollection<Document> = dbItb.getCollection("restaurants")
+        val collectionProducts : MongoCollection<Document> = dbItb.getCollection("products")
+        uploadJSON(collectionRestaurants, "JSON/restaurants.json")
+        uploadJSON(collectionProducts, "JSON/products.json")
+    } catch (e: MongoException) {
+        println("An error occurred: ${e.message}")
+    }
+}
 
-    //Inserting the data from the restaurants JSON
-    val jsonFileRestaurants = File("JSON/restaurants.json")
+// Function to upload the JSON file to the collection
+fun uploadJSON(collection: MongoCollection<Document>, jsonFilePath: String) {
+    val jsonFile = File(jsonFilePath)
     val documents = mutableListOf<Document>()
-    jsonFileRestaurants.forEachLine {restuarant ->
-        val document = Document.parse(restuarant)
+    jsonFile.forEachLine { line ->
+        val document = Document.parse(line)
         documents.add(document)
     }
-    collectionRestaurants.insertMany(documents)
-
-    //Inserting the data from the products JSON
-    val jsonFileProducts = File("JSON/products.json")
-    documents.clear()
-    jsonFileProducts.forEachLine { product ->
-        val document = Document.parse(product)
-        documents.add(document)
-    }
-    collectionProducts.insertMany(documents)
+    collection.insertMany(documents)
 }
